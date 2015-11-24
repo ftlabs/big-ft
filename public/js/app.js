9d4565f0ca87a714c584365a519013c7965c256e
Andrew Betts <andrew.betts@ft.com>
2015-11-24 12:32:40 +0000

    Auto advance main stories

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
+
+function nextMainStory() {
+	['main-stories__story--current', 'main-stories__media--current'].forEach(function(c) {
+		var existing = $('.'+c);
+		existing.removeClass(c).next().addClass(c);
+		if (!$('.'+c).length) {
+			existing.parent().children().eq(0).addClass(c);
+		}
+	});
+}
+
 $(function() {
 	$('.ticker').ticker();
+
+	setInterval(nextMainStory, 5000);
 });
