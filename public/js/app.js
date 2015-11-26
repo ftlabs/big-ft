25036f70a29b1f714367507f3f633fcc80169527
Sean Tracey <seanmtracey@gmail.com>
2015-11-26 17:52:47 +0000

    Clocks update according to timezone offset

--- public/js/app.js
+++ public/js/app.js
@@ -148,10 +148,28 @@ var __bigFT = (function(){
 
 	}
 
+	function updateClocks(){
+		const clocks = document.querySelectorAll('[data-tz]');
+		const hour = moment().hour();
+
+		[].forEach.call(clocks, function(clock){
+
+			const offset = parseInt(clock.getAttribute('data-tz'));
+
+			var adjustedTime = hour + offset;
+
+			clock.innerHTML = moment().hours(adjustedTime).format("HH") + ":" + moment().minute();
+
+		});
+
+	}
+
 	function initialise(){
 
 		updateContent();
+		updateClocks();
 
+		setInterval(updateClocks, 60000);
 		setInterval(updateContent, updateInterval);
 
 	}
