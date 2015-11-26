0a8e32bf4f99c05a60bd7ca6b87f2a76216dd037
Jake Champion <jakechampion.jake2@googlemail.com>
2015-11-26 17:45:31 +0000

    Add search functionality

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router(); //eslint-disable-line new-cap
-const topStories = require('../lib/top-stories');
+const topStories = require('../lib/topStories');
+const search = require('../lib/search');
 const cors = require('../middleware/cors');
 
 router.use(cors);
@@ -12,6 +13,20 @@ router.get('/top-stories', (req, res) => {
   const numberOfArticles = req.query.numberOfArticles;
 
   topStories(startFrom, numberOfArticles)
+  .then(sendJson)
+  .catch(send503);
+})
+
+router.get('/search', (req, res) => {
+  const sendJson = res.json.bind(res);
+  const send503 = res.sendStatus.bind(res,503);
+
+  const searchTerm = req.query.keyword;
+
+  const startFrom = req.query.startFrom;
+  const numberOfArticles = req.query.numberOfArticles;
+
+  search(searchTerm, startFrom, numberOfArticles)
   .then(sendJson)
   .catch(send503);
 })
