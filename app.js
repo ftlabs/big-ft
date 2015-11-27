e75d8aa8c703fd5c4c64ba86aed5d024dbbf29fa
Jake Champion <jakechampion.jake2@googlemail.com>
2015-11-27 10:08:14 +0000

    Making a boilerplate

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -23,7 +24,7 @@ ftwebservice(app, {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(compression({threshold: 0}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
