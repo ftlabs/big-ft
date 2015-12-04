0ba2316ccb89636c43d9e6b39a080661d4ab6fb8
Jake Champion <jakechampion.jake2@googlemail.com>
2015-12-04 15:55:18 +0000

    Report error to sentry for data endpoints

--- routes/data.js
+++ routes/data.js
@@ -1,3 +1,6 @@
+const SENTRY_DSN = process.env.SENTRY_DSN;
+const raven = require('raven');
+const client = new raven.Client(SENTRY_DSN);
 const express = require('express');
 const router = express.Router(); //eslint-disable-line new-cap
 const topStories = require('../lib/topStories');
@@ -6,29 +9,29 @@ const cors = require('../middleware/cors');
 
 router.use(cors);
 router.get('/top-stories', (req, res) => {
-  const sendJson = res.json.bind(res);
-  const send503 = res.sendStatus.bind(res,503);
-
   const startFrom = req.query.startFrom;
   const numberOfArticles = req.query.numberOfArticles;
 
   topStories(startFrom, numberOfArticles)
-  .then(sendJson)
-  .catch(send503);
+  .then(data => res.json(data))
+  .catch(e => {
+    client.captureException(e);
+    res.sendStatus(503);
+  });
 })
 
 router.get('/search', (req, res) => {
-  const sendJson = res.json.bind(res);
-  const send503 = res.sendStatus.bind(res,503);
-
   const searchTerm = req.query.keyword;
 
   const startFrom = req.query.startFrom;
   const numberOfArticles = req.query.numberOfArticles;
 
   search(searchTerm, startFrom, numberOfArticles)
-  .then(sendJson)
-  .catch(send503);
+  .then(data => res.json(data))
+  .catch(e => {
+    client.captureException(e);
+    res.sendStatus(503);
+  });
 })
 
 module.exports = router;
