f630ceb343f60f84fbdb6c8b37adae2d124b4772
Jake Champion <jakechampion.jake2@googlemail.com>
2015-12-04 15:55:24 +0000

    linting

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 const appVersion = require(path.join(__dirname, '..', 'package.json')).version;
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', {
   	version : appVersion
   });
 });
 
-router.get('/should-update', function(req, res){
+router.get('/should-update', function (req, res){
 
 	res.json({
 		version : appVersion
