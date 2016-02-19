2f3e9901edcfd256113d2b4ea8e56a443a52fa8b
Ada Rose Edwards <ada.edwards@ft.com>
2016-02-19 08:01:30 -0800

    Mixin organisations

--- server/lib/topStories.js
+++ server/lib/topStories.js
@@ -3,14 +3,16 @@ const fetch = require('node-fetch');
 const briefArticle = require('./viewModel');
 const R = require('ramda');
 const briefArticleMapper = R.map(briefArticle);
+const bluebird = require("bluebird");
 
 const apiOrigin = process.env.apiOrigin;
 const apiKey = process.env.apiKey;
 const frontPageIdUK = process.env.frontPageId;
 const frontPageIdUS = process.env.frontPageIdUS;
+const co = require('co');
 
 // edition can be "US" or "INTL" (both meaning US), or "UK" (and anything except "US"/"INTL", including null or "", i.e. its the default)
-module.exports = function (startFrom, numberOfArticles, edition) {
+module.exports = function (startFrom, numberOfArticles, edition, organisation) {
 	'use strict';
 
 	if (isNaN(startFrom)) {
@@ -26,13 +28,41 @@ module.exports = function (startFrom, numberOfArticles, edition) {
 		frontPageId = frontPageIdUS;
 	}
 
-	return fetch(`${apiOrigin}/${frontPageId}/main-content?apiKey=${apiKey}`)
+
+	let organisationPromise;
+	if (organisation) {
+
+		organisationPromise = bluebird.coroutine(function * () {
+			const id = yield fetch('https://next.ft.com/search-suggestions?flatten=true&limit=1&exclude=special&q=twitter')
+			.then(response => response.json())
+			.then(json => json[0].id);
+
+			const newApiReq = yield fetch(`http://api.ft.com/content?isAnnotatedBy=${id}&authority=http://api.ft.com/system/FT-TME&bindings=v1&apiKey=${apiKey}`)
+			.then(response => response.json())
+			.then(json => json.content[0].apiUrl)
+
+			const article = yield fetch(newApiReq + `?apiKey=${apiKey}`)
+			.then(response => response.json());
+
+			return article;
+		}).then(article => [article], () => []);
+	} else {
+		organisationPromise = Promise.resolve([]);
+	}
+
+	const articlePromise = fetch(`${apiOrigin}/${frontPageId}/main-content?apiKey=${apiKey}`)
 	.then(response => response.json())
 	.then(data => {
 		const articles = briefArticleMapper(data.pageItems);
 		const articlesWithImages = articles.filter(article => article.image !== '');
 
 		return articlesWithImages.slice(startFrom).slice(0, numberOfArticles);
-	})
-	;
+	});
+
+	return Promise.all(organisationPromise, articlePromise).then(function (organisations, articles) {
+		if (organisations[0]) {
+			articles.unshift(organisations[0]);
+		}
+		return articles;
+	});
 }
