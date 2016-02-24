229d37468e41cae8e541ccfc19263f33db74ad6b
Ada Rose Edwards <ada.edwards@ft.com>
2016-02-23 20:38:43 -0800

    Remove service worker mangling

--- client/src/sw.js
+++ client/src/sw.js
@@ -7,20 +7,4 @@ toolbox.router.default = toolbox.networkFirst;
 toolbox.router.any('/static/*', toolbox.fastest);
 
 // Data should query the network first but fallback to the Cache
-toolbox.router.any('/data/*', function (request) {
-	return toolbox.networkFirst(request)
-	.then(function (response) {
-		
-		// here I will alter the body from an array of values
-		// to an object that looks like:
-		// {
-		//   data: [],
-		//   timestamp: response.headers.get('ft-timestamp')
-		// }
-		// the ft-timestamp header is set by fetch
-		// it is echoed back by the server
-		// 
-		// in case it is not there we do not alter the response
-		return response;
-	});
-});
+toolbox.router.any('/data/*', toolbox.networkFirst);
