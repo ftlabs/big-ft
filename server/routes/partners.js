614356372be07ee70b299c2b45ad7f2d4c8d1f38
Sean Tracey <seanmtracey@gmail.com>
2016-03-17 10:15:36 +0000

    Removed erroneous whitespace

--- server/routes/partners.js
+++ server/routes/partners.js
@@ -4,16 +4,16 @@ const validPartners = require('../lib/partners');
 module.exports = function (req, res){
 
 	const partner = req.query.partner;
-	
+
 	if(partner === undefined || validPartners.check(partner) === false){
-		
+
 		res.json({
 			status : 404,
 			message : `Partner: '${partner}' is not a valid partner.`
 		});
 
 	} else {
-	
+
 		res.json({
 			status : 200,
 			data : {
